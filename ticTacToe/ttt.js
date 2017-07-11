7f05404758931037fead07b60b28cb8715a6e0bd
Navneet Karnam <navneetkarnam@gmail.com>
2017-07-11 15:44:25 -0400

    Version 1.0

---
+++ ticTacToe/ttt.js
@@ -0,0 +1,147 @@
+var b1 = document.querySelector("#b1");
+var b2 = document.querySelector("#b2");
+var b3 = document.querySelector("#b3");
+var b4 = document.querySelector("#b4");
+var b5 = document.querySelector("#b5");
+var b6 = document.querySelector("#b6");
+var b7 = document.querySelector("#b7");
+var b8 = document.querySelector("#b8");
+var b9 = document.querySelector("#b9");
+
+var x = false;
+
+b1.addEventListener("click",function(){
+	if(!x){
+		b1.textContent = "X";
+		b1.style.fontSize = "50px";
+		b1.style.textAlign = "center";
+		x = true;
+	}
+	else{
+		b1.textContent = "O";
+		b1.style.fontSize = "50px";
+		b1.style.textAlign = "center";
+		x = false;
+	}
+});
+
+b2.addEventListener("click",function(){
+	if(!x){
+		b2.textContent = "X";
+		b2.style.fontSize = "50px";
+		b2.style.textAlign = "center";
+		x = true;
+	}
+	else{
+		b2.textContent = "O";
+		b2.style.fontSize = "50px";
+		b2.style.textAlign = "center";
+		x = false;
+	}
+});
+
+b3.addEventListener("click",function(){
+	if(!x){
+		b3.textContent = "X";
+		b3.style.fontSize = "50px";
+		b3.style.textAlign = "center";
+		x = true;
+	}
+	else{
+		b3.textContent = "O";
+		b3.style.fontSize = "50px";
+		b3.style.textAlign = "center";
+		x = false;
+	}
+});
+
+
+b4.addEventListener("click",function(){
+	if(!x){
+		b4.textContent = "X";
+		b4.style.fontSize = "50px";
+		b4.style.textAlign = "center";
+		x = true;
+	}
+	else{
+		b4.textContent = "O";
+		b4.style.fontSize = "50px";
+		b4.style.textAlign = "center";
+		x = false; 
+	}
+});
+
+b5.addEventListener("click",function(){
+	if(!x){
+		b5.textContent = "X";
+		b5.style.fontSize = "50px";
+		b5.style.textAlign = "center";
+		x = true;
+	}
+	else{
+		b5.textContent = "O";
+		b5.style.fontSize = "50px";
+		b5.style.textAlign = "center";
+		x = false; 
+	}
+});
+
+b6.addEventListener("click",function(){
+	if(!x){
+		b6.textContent = "X";
+		b6.style.fontSize = "50px";
+		b6.style.textAlign = "center";
+		x = true;
+	}
+	else{
+		b6.textContent = "O";
+		b6.style.fontSize = "50px";
+		b6.style.textAlign = "center";
+		x = false; 
+	}
+});
+
+b7.addEventListener("click",function(){
+	if(!x){
+		b7.textContent = "X";
+		b7.style.fontSize = "50px";
+		b7.style.textAlign = "center";
+		x = true;
+	}
+	else{
+		b7.textContent = "O";
+		b7.style.fontSize = "50px";
+		b7.style.textAlign = "center";
+		x = false; 
+	}
+});
+
+b8.addEventListener("click",function(){
+	if(!x){
+		b8.textContent = "X";
+		b8.style.fontSize = "50px";
+		b8.style.textAlign = "center";
+		x = true;
+	}
+	else{
+		b8.textContent = "O";
+		b8.style.fontSize = "50px";
+		b8.style.textAlign = "center";
+		x = false; 
+	}
+});
+
+b9.addEventListener("click",function(){
+	if(!x){
+		b9.textContent = "X";
+		b9.style.fontSize = "50px";
+		b9.style.textAlign = "center";
+		x = true;
+	}
+	else{
+		b9.textContent = "O";
+		b9.style.fontSize = "50px";
+		b9.style.textAlign = "center";
+		x = false; 
+	}
+});
